6db3582130d6de8f6d72077a039a9cbc7f4b658d
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-01-12 17:46:14 +0530

    Add Custom Logger

--- whatsapp backend/src/index.js
+++ whatsapp backend/src/index.js
@@ -1,9 +1,10 @@
 import app from "./app.js";
+import logger from "./configs/logger.config.js";
 
 
 //env variables
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`server is listening at ${PORT}`);
+    logger.info(`server is listening at ${PORT}`);
 });
