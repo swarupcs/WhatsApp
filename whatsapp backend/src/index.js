d9164884cbaa84f5f240928159b785c0bdd8d7d0
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-01-12 18:41:18 +0530

    Handling http errors

--- whatsapp backend/src/index.js
+++ whatsapp backend/src/index.js
@@ -5,6 +5,37 @@ import logger from "./configs/logger.config.js";
 //env variables
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+let server;
+
+server = app.listen(PORT, () => {
     logger.info(`server is listening at ${PORT}`);
+});
+
+
+//handle server errors
+
+const exitHandler = () => {
+    if(server) {
+        logger.info("Server closed.");
+        process.exit(1);
+    } else {
+       process.exit(1); 
+    }
+}
+
+
+const unexpectedErrorHandler = (error) => {
+    logger.error(error);
+    exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM", () => {
+     if(server) {
+        logger.info("Server closed");
+        process.exit(1);
+     }
 });
