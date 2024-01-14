05284dfc9c2074ae3a32540e24b52b0f448ba223
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-01-14 10:58:08 +0530

    Use redux persist to rehydrate only certain values

--- whatsapp_frontend/src/app/store.js
+++ whatsapp_frontend/src/app/store.js
@@ -1,11 +1,35 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+//slices
 import userSlice from "../features/userSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import createFilter from "redux-persist-transform-filter";
+
+//saveUserOnlyFilter
+const saverUserOnlyFilter = createFilter("user", ["user"]);
+
+
+//persist config
+const persistConfig = {
+    key: "user",
+    storage,
+    whitelist: ["user"],
+    transforms: [saverUserOnlyFilter],
+};
 
 const rootReducer = combineReducers({
     user: userSlice,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
     devTools: true,
 });
+
+export const peristor = persistStore(store);
