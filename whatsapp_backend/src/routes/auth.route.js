4c88d94261a0febd745687861a34f0c111f9b9a0
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-01-14 22:51:44 +0530

    Auth middleware explained in details

--- whatsapp_backend/src/routes/auth.route.js
+++ whatsapp_backend/src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import trimRequest from "trim-request";
 import { login, logout, refreshToken, register } from "../controllers/auth.controller.js";
+import authMiddleware from "../middleswares/authMiddleware.js";
 
 const router = express.Router();
 
@@ -8,6 +9,9 @@ router.route("/register").post(trimRequest.all, register);
 router.route("/login").post(trimRequest.all, login);
 router.route("/logout").post(trimRequest.all, logout);
 router.route("/refreshtoken").post(trimRequest.all, refreshToken);
+router.route("/testingauthMiddleware").get(trimRequest.all, authMiddleware, (req, res) => {
+    res.send(req.user);
+}); 
 
 
 export default router; 
