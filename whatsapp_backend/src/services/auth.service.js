50111adc5e2ec858d2c07230149c6fa126eacb6f
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-01-14 21:52:58 +0530

    Login and logout functions

--- whatsapp_backend/src/services/auth.service.js
+++ whatsapp_backend/src/services/auth.service.js
@@ -1,6 +1,7 @@
 import createHttpError from "http-errors";
 import validator from "validator";
 import { UserModel } from "../models/index.js";
+import bcrypt from "bcrypt";
 
 //env variables
 const { DEFAULT_PICTURE, DEFAULT_STATUS} = process.env;
@@ -65,4 +66,23 @@ export const createUser = async (userData) => {
   }).save();
 
   return user;
+};
+
+export const signUser = async(email, password) => {
+  const user = await UserModel.findOne({email: email.toLowerCase()}). lean();
+
+  //check if user exist
+  if(!user) {
+    throw createHttpError.NotFound("Invalid credentials.");
+  } 
+
+  //compare passwords
+  let passwordMatches = await bcrypt.compare(password, user.password);
+
+  if(!passwordMatches) {
+    throw createHttpError.NotFound("Invalid credentials");
+  }
+
+  return user;
+
 };
