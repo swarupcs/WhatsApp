b37c4311ab53faa0d6ebf95d704f3ecad27490e8
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-01-16 22:50:38 +0530

    setup the register and login page and it's form components

--- whatsapp_frontend/src/pages/login.js
+++ whatsapp_frontend/src/pages/login.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import LoginForm from '../components/auth/LoginForm'
 
 function login() {
   return (
-    <div>
-      Login
+    <div className='h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden'>
+      {/* Container */}
+      <div className='flex w-[1600px] mx-auto h-full'>
+      {/* Login form */}
+      <LoginForm/>
+      </div>
     </div>
   )
 }
