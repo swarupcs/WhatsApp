1093a144974719ec01f9244137ad448984b4c1c3
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-01-19 10:49:59 +0530

    Create custom inputs with validation

--- whatsapp_frontend/src/components/auth/RegisterForm.jsx
+++ whatsapp_frontend/src/components/auth/RegisterForm.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import {useForm} from "react-hook-form";
+import React from "react";
+import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { signUpSchema } from '../../utils/validation';
+import { signUpSchema } from "../../utils/validation";
+import AuthInput from "./AuthInput";
 
 function RegisterForm() {
   const {
@@ -17,24 +18,50 @@ function RegisterForm() {
   console.log("values", watch());
   console.log("errors", errors);
   return (
-    <div className='h-screen w-full flex items-center justify-center overflow-hidden'>
+    <div className="h-screen w-full flex items-center justify-center overflow-hidden">
       {/* Container */}
-      <div className='max-w-md space-y-8 p-10 dark:bg-dark_bg_2 rounded-xl'>
+      <div className="max-w-md space-y-8 p-10 dark:bg-dark_bg_2 rounded-xl">
         {/* Heading */}
-        <div className='text-center dark:text-dark_text_1'>
-            <h2 className='mt-6 text-3xl font-bold'>Welcome</h2>
-            <p className='mt-2 text-sm'>Sign up</p>
+        <div className="text-center dark:text-dark_text_1">
+          <h2 className="mt-6 text-3xl font-bold">Welcome</h2>
+          <p className="mt-2 text-sm">Sign up</p>
         </div>
         {/* Form */}
-        <form
-        onSubmit={handleSubmit(onSubmit)}
-         className='mt-6 space-y-6'>
-          <input type="text" {...register("name", { required: true })} />
-          <button type='submit'>submit</button>
-         </form>
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-6 space-y-6">
+          <AuthInput
+            name="name"
+            type="text"
+            placeholder="Full Name"
+            register={register}
+            error={errors?.name?.message}
+          />
+          <AuthInput
+            name="email"
+            type="text"
+            placeholder="Email Address"
+            register={register}
+            error={errors?.email?.message}
+          />
+          <AuthInput
+            name="status"
+            type="text"
+            placeholder="Status"
+            register={register}
+            error={errors?.status?.message}
+          />
+          <AuthInput
+            name="password"
+            type="password"
+            placeholder="Password"
+            register={register}
+            error={errors?.password?.message}
+          />
+
+          <button type="submit">submit</button>
+        </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default RegisterForm
+export default RegisterForm;
