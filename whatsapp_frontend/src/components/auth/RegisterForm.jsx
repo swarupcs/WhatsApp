b2850eb6b8e5262c0b36ad2b7803d8162686f1ee
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-01-20 16:58:27 +0530

    Upload image to cloudinary

--- whatsapp_frontend/src/components/auth/RegisterForm.jsx
+++ whatsapp_frontend/src/components/auth/RegisterForm.jsx
@@ -4,10 +4,14 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { signUpSchema } from "../../utils/validation";
 import AuthInput from "./AuthInput";
 import { useDispatch, useSelector } from "react-redux";
-import  PulseLoader  from "react-spinners/PulseLoader";
+import PulseLoader from "react-spinners/PulseLoader";
 import { Link, useNavigate } from "react-router-dom";
-import { registerUser } from "../../features/userSlice";
+import { registerUser, changeStatus } from "../../features/userSlice";
 import Picture from "./Picture";
+import axios from "axios";
+
+const cloud_name = process.env.REACT_APP_CLOUD_NAME;
+const cloud_secret = process.env.REACT_APP_CLOUD_SECRET;
 
 function RegisterForm() {
   const dispatch = useDispatch();
@@ -27,15 +31,42 @@ function RegisterForm() {
   });
 
   const onSubmit = async (data) => {
-    let res = await dispatch(registerUser({ ...data, picture : ""}));
-    // console.log(res);
-    if(res.payload.user) {
-      navigate("/");
+    dispatch(changeStatus("loading"));
+    if (picture) {
+      //upload to cloudinary and then register user
+      await uploadImage().then(async (response) => {
+        let res = await dispatch(
+          registerUser({ ...data, picture: response.secure_url })
+        );
+        if (res?.payload?.user) {
+          navigate("/");
+        }
+      });
+    } else {
+      let res = await dispatch(registerUser({ ...data, picture: "" }));
+      if (res?.payload?.user) {
+        navigate("/");
+      }
+      // console.log(res);
     }
+
   };
   // console.log(picture, readablePicture);
   // console.log("values", watch());
   // console.log("errors", errors);
+
+  const uploadImage = async () => {
+    let formData = new FormData();
+    formData.append("upload_preset", cloud_secret);
+    formData.append("file", picture);
+
+    const { data } = await axios.post(
+      `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
+      formData
+    );
+    console.log(data);
+    return data;
+  };
   return (
     <div className="min-h-screen w-full flex items-center justify-center overflow-hidden">
       {/* Container */}
@@ -75,25 +106,38 @@ function RegisterForm() {
             register={register}
             error={errors?.password?.message}
           />
-          {/* Picture */} 
-          <Picture readablePicture={readablePicture} setReadablePicture={setReadablePicture} setPicture={setPicture}/>
+          {/* Picture */}
+          <Picture
+            readablePicture={readablePicture}
+            setReadablePicture={setReadablePicture}
+            setPicture={setPicture}
+          />
           {/* If we have an error */}
-          {
-            error ? ( <div>
+          {error ? (
+            <div>
               <p className="text-red-400">{error}</p>
-            </div> ) : null
-          }
+            </div>
+          ) : null}
           {/* Submit button */}
-          <button 
-          className="w-full flex justify-center bg-green_1 text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none hover:bg-green_2 shadow-lg cursor-pointer transition ease-in duration-300"
-
-          type="submit">
-          {status === "loading" ? <PulseLoader color="#fff" size={16}/> : "Sign up"}
+          <button
+            className="w-full flex justify-center bg-green_1 text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none hover:bg-green_2 shadow-lg cursor-pointer transition ease-in duration-300"
+            type="submit"
+          >
+            {status === "loading" ? (
+              <PulseLoader color="#fff" size={16} />
+            ) : (
+              "Sign up"
+            )}
           </button>
           {/* Sign in link */}
           <p className="flex flex-col items-center justify-center mt-10 text-center text-md dark:text-dark_text_1">
             <span>have an account</span>
-            <Link href="/login" className="hover:underline cursor-pointer transition ease-in duration-300">Sign in</Link>
+            <Link
+              href="/login"
+              className="hover:underline cursor-pointer transition ease-in duration-300"
+            >
+              Sign in
+            </Link>
           </p>
         </form>
       </div>
--- whatsapp_frontend/src/features/userSlice.js
+++ whatsapp_frontend/src/features/userSlice.js
@@ -41,6 +41,9 @@ export const userSlice = createSlice({
                 token: "",
             };
         },
+        changeStatus:(state, action) => {
+            state.status = action.payload;
+        }
     },
     extraReducers(builder) {
         builder.addCase(registerUser.pending, (state, action) => {
@@ -60,6 +63,6 @@ export const userSlice = createSlice({
 });
 
 
-export const { logout } = userSlice.actions;
+export const { logout, changeStatus } = userSlice.actions;
 
 export default userSlice.reducer;
